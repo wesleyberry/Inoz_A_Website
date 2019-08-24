b0a1c26f2ae91d47b50f7d5e5843278f58baff07
Wesley Berry <wesleyberry52@gmail.com>
2019-08-24 13:31:29 -0500

    Finished About Effects

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,41 @@
 window.sr = ScrollReveal();
+// Landing Page
 sr.reveal('#landingPageLogo', {
     duration: 1000, 
+    origin: 'bottom',
+    distance: '500px',
+});
+// About Us
+sr.reveal('#firstAboutImage', {
+    duration: 750, 
+    origin: 'bottom',
+    distance: '500px',
+});
+sr.reveal('#secondAboutImage', {
+    duration: 750, 
+    delay: 250,
+    origin: 'bottom',
+    distance: '500px',
+});
+sr.reveal('#thirdAboutImage', {
+    duration: 750,
+    delay: 250, 
+    origin: 'bottom',
+    distance: '500px',
+});
+sr.reveal('#aboutText1', {
+    duration: 750,
+    origin: 'bottom',
+    distance: '500px',
+});
+sr.reveal('#aboutText2', {
+    duration: 750,
+    origin: 'bottom',
+    distance: '500px',
+});
+sr.reveal('#aboutText3', {
+    duration: 750,
+    delay: 250, 
     origin: 'bottom',
     distance: '500px',
 });
