77271287d7c282faf3fffb3c47ebfec2e6ed9fef
Wesley Berry <wesleyberry52@gmail.com>
2019-08-29 11:07:38 -0500

    set up database connection

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 require('dotenv').config();
+const path = require('path');
+const express = require('express');
+
+const session = require("express-session");
+// const passport = require("./config/passport");
+
+const db = require('./models');
+// const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// app.use(routes);
+
+const syncOptions = { force: false };
+
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = false;
+}
+
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
+
+module.exports = app;
