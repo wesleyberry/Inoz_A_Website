59c4daebfee9a0ca92c9c5734091337ef84a17c2
Wesley Berry <wesleyberry52@gmail.com>
2019-09-15 20:26:12 -0500

    removed animations for images

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -6,31 +6,31 @@ sr.reveal('#landingPageLogo', {
     distance: '500px',
 });
 // About Us
-sr.reveal('#secondAboutImage', {
-    duration: 750, 
-    delay: 250,
-    origin: 'bottom',
-    distance: '500px',
-});
-sr.reveal('#thirdAboutImage', {
-    duration: 750,
-    delay: 250, 
-    origin: 'bottom',
-    distance: '500px',
-});
-sr.reveal('#aboutText1', {
-    duration: 750,
-    origin: 'bottom',
-    distance: '500px',
-});
-sr.reveal('#aboutText2', {
-    duration: 750,
-    origin: 'bottom',
-    distance: '500px',
-});
-sr.reveal('#aboutText3', {
-    duration: 750,
-    delay: 250, 
-    origin: 'bottom',
-    distance: '500px',
-});
+// sr.reveal('#secondAboutImage', {
+//     duration: 750, 
+//     delay: 250,
+//     origin: 'bottom',
+//     distance: '500px',
+// });
+// sr.reveal('#thirdAboutImage', {
+//     duration: 750,
+//     delay: 250, 
+//     origin: 'bottom',
+//     distance: '500px',
+// });
+// sr.reveal('#aboutText1', {
+//     duration: 750,
+//     origin: 'bottom',
+//     distance: '500px',
+// });
+// sr.reveal('#aboutText2', {
+//     duration: 750,
+//     origin: 'bottom',
+//     distance: '500px',
+// });
+// sr.reveal('#aboutText3', {
+//     duration: 750,
+//     delay: 250, 
+//     origin: 'bottom',
+//     distance: '500px',
+// });
